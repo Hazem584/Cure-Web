c10ab577f03a13e0e249e058449afbd48925a022
miroElbehery <miroelbehery178@gmail.com>
2025-10-16 04:15:23 +0300

    1-footer modified in responsivenise become more responsive 2-NavBar Design Done 3-Added Primary color to tailwind.config

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { BsHeartPulse } from "react-icons/bs";
+import SearchBar from "./components/SearchBar";
+import ProfileDropdown from "./components/ProfileDropdown";
 const NavBar = () => {
-  return (
-    <div>
-      NavBar
-    </div>
-  )
-}
+     return (
+          <div className="navbar bg-base-100 h-28 justify-between shadow-sm px-10">
+               <div className="logo">
+                    <BsHeartPulse className="text-primary text-3xl" />
+               </div>
 
-export default NavBar
+               <SearchBar />
+               {/* profile */}
+               <ProfileDropdown />
+          </div>
+     );
+};
+
+export default NavBar;
