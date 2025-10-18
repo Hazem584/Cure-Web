690c5de2dfe22f4d586ec92e301c58ed4c53e681
saramuhammed-22 <saramuhammed@gmail.com>
2025-10-18 13:09:19 +0300

    add profile

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NotFound from "./components/not_found/NotFound";
 import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
 import Home from "./pages/home/Home";
-
+import Profile from "./pages/profile/Profile"
 const App = () => {
   return (
       <Routes>
@@ -12,6 +12,7 @@ const App = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<Login />} />
+        <Route path="/Profile" element={<Profile/>}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
   );
