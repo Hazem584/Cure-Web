fafd8a822d725d075292ca53cce88452a48839b5
Hazem Mohamed <hazemhkcyy@gmail.com>
2025-10-18 21:46:34 +0300

    Add Reviews and Ratings feature with modal for user feedback; refactor App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,22 @@ import SignUp from "./pages/auth/SignUp";
 import Home from "./pages/home/Home";
 import Booking from "./pages/Booking/Booking";
 import Appointments from "./pages/appointments/Appointments";
-import NavBar from "./components/header/NavBar"
+import NavBar from "./components/header/NavBar";
+import Footer from "./components/footer/Footer";
 const App = () => {
   return (
     <>
-         <NavBar/> 
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/booking" element={<Booking />} />
-      <Route path="/appointments" element={<Appointments />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes> 
-</>
-
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/appointments" element={<Appointments />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
 
--- src/pages/appointments/Appointments.jsx
+++ src/pages/appointments/Appointments.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FaStar, FaRegStar } from "react-icons/fa";
 import BookingCalendar from "./components/BookingCalendar";
 import DoctorDetails from "./components/DoctorDetails";
+import ReviewsAndRating from "./components/ReviewsAndRating";
 
 const NORMALIZED_DAY_OPTIONS = { weekday: "short" };
 
@@ -78,21 +79,25 @@ const Appointments = () => {
 
     return () => clearInterval(interval);
   }, []);
-
   return (
-    <div className="min-h-screen bg-slate-50 py-12 ">
-      <div className=" flex max-w-full flex-col gap-8 lg:flex-row px-20">
-        <div className="flex-1 space-y-6">
-          <BookingCalendar
-            dates={dates}
-            timeSlots={timeSlots}
-            selectedDate={selectedDate}
-            selectedTime={selectedTime}
-            onSelectDate={setSelectedDate}
-            onSelectTime={setSelectedTime}
-          />
+    <div className="min-h-screen bg-slate-50 py-8 px-4 sm:px-6 lg:px-12 max-[300px]:px-2 max-[300px]:py-6">
+      <div className="mx-auto">
+        <div className="flex flex-col gap-8 lg:flex-row max-[300px]:gap-6">
+          <div className="order-1 lg:order-2 lg:flex-shrink-0">
+            <DoctorDetails />
+          </div>
+          <div className="order-2 flex flex-col gap-8 lg:order-1 lg:flex-1">
+            <BookingCalendar
+              dates={dates}
+              timeSlots={timeSlots}
+              selectedDate={selectedDate}
+              selectedTime={selectedTime}
+              onSelectDate={setSelectedDate}
+              onSelectTime={setSelectedTime}
+            />
+            <ReviewsAndRating />
+          </div>
         </div>
-        <DoctorDetails />
       </div>
     </div>
   );
