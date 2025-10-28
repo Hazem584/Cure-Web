8944903cb9d1a98934a0a08c9d735110132d6fcf
Ayad Emad <ayad.emad144@gmail.com>
2025-10-28 17:26:46 +0300

    Add dashboard layout with doctor list and delete confirmation feature

--- src/AdminLayout/AdminLayout.jsx
+++ src/AdminLayout/AdminLayout.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import NavBar from "../components/header/NavBar";
 
 const AdminLayout = () => {
   return (
     <>
-      <Routes></Routes>
+      <NavBar />
+      <Routes>
+        <Route index element={<Dashboard />} />
+      </Routes>
     </>
   );
 };
