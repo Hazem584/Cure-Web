e51b1ddb477633486dd42121aa97ad5a91eace89
hamada12389 <hamadakhaledgithub@gmail.com>
2025-10-30 23:11:56 +0200

    fix doctors

--- src/UserLayout/pages/doctors/Doctors.jsx
+++ src/UserLayout/pages/doctors/Doctors.jsx
@@ -74,7 +74,7 @@ const Doctors = () => {
   return (
     <div className=" dark:bg-dark-darkBg ">
       <NavBar />
-      <div className=" flex justify-center items-center ">
+      <div className=" mr-10 ml-10  ">
         <Top onToggleFilter={toggleFilter} />
         <div className="flex flex-row [@media(max-width:1400px)]:flex-col mt-10  ">
           {isFilterOpen && <FilterOptions />}
