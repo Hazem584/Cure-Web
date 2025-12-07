eb2a67d30041d6743a7c2ee9c3255904cc85b2f5
Ayad Emad <ayad.emad144@gmail.com>
2025-12-08 00:44:29 +0200

    Refactor Booking components to handle doctor data safely; update Card and CardButtons to use optional chaining and change appointment deletion to cancellation.

--- src/UserLayout/pages/Booking/components/Card.jsx
+++ src/UserLayout/pages/Booking/components/Card.jsx
@@ -8,6 +8,7 @@ const Card = ({
 }) => {
   const date = new Date(appointmentDate);
   const formatted = format(date, "EEEE, MMMM dd ");
+
   return (
     <div className="border border-[#BBC1C7] dark:border-dark-borderDark  flex flex-col p-2 rounded-2xl   max-[639px]:w-11/12  ">
       <div className="flex justify-between items-center mt-3 gap-4 ">
@@ -38,20 +39,20 @@ const Card = ({
       </div>
       <hr className=" border-gray-300 my-2 w-[97%] " />
       <div className="flex gap-3 px-2">
-        <Avatar src={doctor.image} alt="avatar" />
+        <Avatar src={doctor?.image} alt="avatar" />
         <div className="flex flex-col">
           <h1 className="text-[#33384B] font-semibold dark:text-dark-textOnDark">
-            {doctor.name}
+            {doctor?.name}
           </h1>
           <h1 className="text-[#6D7379] dark:text-dark-textSecondary">
-            {doctor.specialty}
+            {doctor?.specialty}
           </h1>
         </div>
       </div>
       <div className="flex gap-3 px-3">
         <img src="/location-icon.svg" alt="location" />
         <h1 className="mt-2 text-[#6D7379] dark:text-dark-textSecondary">
-          {doctor.address}
+          {doctor?.address}
         </h1>
       </div>
       <div className="mt-4">
@@ -59,7 +60,7 @@ const Card = ({
           status={status}
           id={id}
           refetch={refetch}
-          doctorId={doctor.id}
+          doctorId={doctor?.id}
         />
       </div>
     </div>
